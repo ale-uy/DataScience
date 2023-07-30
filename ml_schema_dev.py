413b0a150e66b6f7701ea3c660691b55eb3695b9
ale-uy <lasm1984@gmail.com>
2023-07-30 02:54:37 -0300

    .

--- ml_schema_dev.py
+++ ml_schema_dev.py
@@ -19,7 +19,7 @@ from sklearn.impute import KNNImputer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score, train_test_split
 from sklearn.metrics import accuracy_score, mean_squared_error, confusion_matrix, \
-    classification_report, precision_score, recall_score, f1_score, roc_auc_score
+    precision_score, recall_score, f1_score, roc_auc_score
 from statsmodels.stats.outliers_influence import summary_table
 
 pd.set_option('display.max_colwidth', None) # Mostrar todo el ancho de las celdas en el DataFrame
